e5d66d6c33abb3041aceb80a86892fefacf700a8
Daniel Batista <dani_batista20@hotmail.com>
2025-02-12 23:12:08 +0000

    start: 10-advance-filtering => Sidebar

--- 03-RefreshProjects/10-advance-filtering/src/App.jsx
+++ 03-RefreshProjects/10-advance-filtering/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar'
 
 const App = () => {
   return (
@@ -7,6 +8,12 @@ const App = () => {
       text-center bg-gray-800 text-white"
     >
       <Header />
+      <div className="container flex flex-row">
+        <Sidebar />
+        <main className="p-5">
+          <h1 className="text-3xl font-bold mb-5">Products</h1>
+        </main>
+      </div>
     </div>
   )
 }
